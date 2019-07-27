6f16ea5b9fc4101be1edddccd5dbae83092d2a87
Asen_G <assen.georgiew@gmail.com>
2019-07-26 22:59:46 -0700

    Initial_commit_Check_log

--- Log_tracker/log_tracker.py
+++ Log_tracker/log_tracker.py
@@ -1,3 +1,27 @@
 """
 The purpose of this script is to query logs and alert if diff.
 """
+from netmiko import Netmiko
+import time
+# from getpass import getpass
+
+cisco1 = {
+    "host": "route-views.routeviews.org",
+    "username": "rviews",
+    "password": "",  # getpass(),
+    "device_type": "cisco_ios",
+}
+
+net_connect = Netmiko(**cisco1)
+command = "show ip int brief"
+
+print()
+print(net_connect.find_prompt())
+
+for x in range(5):
+    output = net_connect.send_command(command)
+    print(output)
+    print(type(output))
+    print()
+    time.sleep(10)
+net_connect.disconnect()
