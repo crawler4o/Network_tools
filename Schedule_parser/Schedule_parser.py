1f1f70ffd6acf769a3615acc416c1f30327ad5be
Asen_G <assen.georgiew@gmail.com>
2020-03-02 05:05:16 -0800

    schedule parser_v2

--- Schedule_parser/Schedule_parser.py
+++ Schedule_parser/Schedule_parser.py
@@ -15,10 +15,10 @@ with open('Data\etmTnsMonth.jsp.html') as file:
                 pass
             elif '1900N' in shift:  # night shifts
                 shifts.append({'start_date': start_date, 'end_date': next_date_str, 'start_time': '19:00', \
-                                                'end_time': '07:00', 'shift': shift, })
+                                                'end_time': '07:00', 'shift': shift[4:], })
             else:
                 shifts.append({'start_date': start_date, 'end_date': start_date, 'start_time': '07:00', \
-                               'end_time': '19:00', 'shift': shift, })
+                               'end_time': '19:00', 'shift': shift[4:], })
 
 with open('import_data.csv', 'w') as file:
     file.write('Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private\n')
